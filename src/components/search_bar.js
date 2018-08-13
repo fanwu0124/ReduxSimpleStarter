67c9595d8c595e101dbaadd2339765ff2bb1e194
Fan Wu <fanwu0124@gmail.com>
2018-08-13 18:18:48 -0400

    State of Search Bar Class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,9 +9,26 @@ const SearchBar1 = () => {
 //Create a SearchBar class component instead of the Functional component.
 //Use class if need to add more functionalities.
 class SearchBar extends React.Component {
+  constructor(props) { //Automatically called when an instance is created.
+    super(props); //super is in the constructor of base class Component.
+
+    //state is a JavaScript object used to record or react the user events.
+    //Each class has its own state object.
+    //Whenever the state of a component is changed, render() and its children
+    //will be rerendered.
+    this.state = { term: '' }; //Initialize state object
+  }
+
   render() {
     //return <input onChange = {this.onInputChange} />; //A separate event handler
-    return <input onChange = {event => console.log(event.target.value)} />;
+
+    //Whenever input is changed, assigned input value to SearchBar's state object.
+    return (
+      <div>
+        <input onChange = {event => this.setState({term: event.target.value})} />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 
   //A separate Event Handler
