0607f68bdd7ae5ec960248b6ca6ae5cc1d7cafd0
Fan Wu <fanwu0124@gmail.com>
2018-08-13 17:49:23 -0400

    Event Handler on the Search Bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,8 +10,14 @@ const SearchBar1 = () => {
 //Use class if need to add more functionalities.
 class SearchBar extends React.Component {
   render() {
-    return <input />;
+    //return <input onChange = {this.onInputChange} />; //A separate event handler
+    return <input onChange = {event => console.log(event.target.value)} />;
   }
+
+  //A separate Event Handler
+  //onInputChange(event) {
+    //console.log(event.target.value);
+  //}
 }
 
 export default SearchBar;
