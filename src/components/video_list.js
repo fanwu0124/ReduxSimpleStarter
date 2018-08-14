c47f0a0c71391f87fc7e94967eccf22005610cb3
Fan Wu <fanwu0124@gmail.com>
2018-08-14 12:12:34 -0400

    Created video_list_item. Generated a list of videos.

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => { //props contains 'videos' from App's state
+  const videoItems = props.videos.map((video) => { //map works like foreach
+    //For each video, create a VideoListItem instance.
+    //Pase {video} to props of VideoListItem.
+    //Create a <li> for each video.
+    //Each video from YouTube API response has a unique etag.
+    //Use etag as each <li>'s unique id.
+    return <VideoListItem key = {video.etag} video = {video} />;
+  });
   return (
     <ul className="col-md-4 list-group">
-      { props.videos.length }
+      {videoItems}
     </ul>
   );
 };
